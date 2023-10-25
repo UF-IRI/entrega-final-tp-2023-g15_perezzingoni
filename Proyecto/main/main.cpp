871b74bcc1803b1b92339b9ccd98c4ae9fd341c6
justina-zingoni <143182788+justina-zingoni@users.noreply.github.com>
2023-10-25 17:08:22 -0300

    abro archivo (intento)

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -1,9 +1,18 @@
+#include "libreria_global.h"
+#include "clientes.h"
+#include "clases.h"
+#include "libreria.h"
+#include "archivos.h"
+#include "iostream"
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
     ifstream infile("asistencias_1697673600000.dat", ios::binary);
     if (!infile.is_open()) {
         cout << "Error opening binary file" << endl;
         return 1;
     }
-
+    infile.close("asistencias_1697673600000.dat");
     return 0;
 }
