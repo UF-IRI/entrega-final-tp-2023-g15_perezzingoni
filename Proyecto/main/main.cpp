126551cc7b69aa4de617b2916eae62c91ef633b7
CamilaPerCan <143199014+CamilaPerCan@users.noreply.github.com>
2023-11-22 18:50:23 -0300

    .

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -8,40 +8,40 @@ int main() {
     //Apertura de Archivos para Lectura
     sClase *misClases;
     ifstream miArchivo1;
-    miArchivo1.open("..\\..\\Proyecto\\Dataset TP\\iriClasesGYM.csv");
+    miArchivo1.open("..\iriClasesGYM.csv");
     if (!miArchivo1.is_open()){
         cout << "Error opening csv file" << endl;
         return 1;
     }
-    eLeerArchivoClases lecturaClase;
+    eResultados lecturaClase;
     lecturaClase = LeerArchivoClases(miArchivo1, &misClases);
-    if(lecturaClase==ErrLeerArchivoClases)
+    if(lecturaClase==ErrEspacio)
         return -1;
     miArchivo1.close();
 
     sClientes Clientela;
     ifstream miArchivo2;
-    miArchivo2.open("..\\..\\Proyecto\\Dataset TP\\iriClientesGYM.csv");
+    miArchivo2.open("..\iriClientesGYM.csv");
     if (!miArchivo2.is_open()){
         cout << "Error opening csv file" << endl;
         return 1;
     }
-    eLeerArchivoClientes lecturaCliente;
+    eResultados lecturaCliente;
     lecturaCliente = LeerArchivoClientes (miArchivo2, Clientela);
-    if(lecturaCliente ==ErrLeerArchivoClientes)
+    if(lecturaCliente ==ErrEspacio)
         return -1;
     miArchivo2.close();
 
 
     sAsistencias Asistencias;
-    ifstream infile("..\\..\\Proyecto\\Dataset TP\\asistencias_1697673600000.dat", ios::binary);
+    ifstream infile("..\asistencias_1697673600000.dat", ios::binary);
     if (!infile.is_open()) {
         cout << "Error opening binary file" << endl;
         return 1;
     }
-    eLeerArchivoAsistencias verificar;
+    eResultados verificar;
     verificar= LeerArchivoAsistencias(infile, Asistencias);
-    if(verificar==ErrLeerArchivoAsistencias)
+    if(verificar==ErrEspacio)
         return -1;
     infile.close();
 
