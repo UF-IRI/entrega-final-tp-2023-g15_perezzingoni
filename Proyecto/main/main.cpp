a5a41ff9b272c6c5b9263fd548de9bdb71b3c2d3
CamilaPerCan <143199014+CamilaPerCan@users.noreply.github.com>
2023-11-23 02:55:59 -0300

    main -Modificar Cliente pt1-

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -53,33 +53,55 @@ int main() {
         cout<< "Ingrese (con un número) lo que desea hacer"<< endl << "1. Agregar un Cliente"<< endl << "2. Modificar un Cliente"<< endl;
         cout<< "3. Reservar una clase" << endl << "4. Desanotarde de una clase" << endl << "5. Finlaizar" << endl;
         cin>>variable;
-        if(variable < 5){
-            switch(variable){
-            case 1:
-                sCliente nuevo;
-                cout<<"Ingrese su nombre";
-                cin>>nuevo.nombre;
-                cout<<"Ingrese su apellido";
-                cin>>nuevo.apellido;
-                cout<<"Ingrese su email";
-                cin>>nuevo.email;
-                cout<<"Ingrese su telefono";
-                cin>>nuevo.telefono;
-                cout<<"Ingrese su fecha de nacimiento";
-                cin>>nuevo.fechaNac;
-                nuevo.idCliente=Clientela.CantClientes+1;
-                nuevo.estado=0;
-                eResultados verificar;
-                verificar= AgregarCliente(Clientela, nuevo);
-                if(verificar==Exito)
-                    cout<< "La operacion se realizó correctamente"<< endl;
-                        else {
-                            if(verificar==ErrExiste)
-                            cout<<"El cliente ya exite"<<endl;
-            case 2:
-            case 3:
-            case 4:
+        if(variable==1){//Agregar nuevo cliente
+            sCliente nuevo;
+            cout<<"Ingrese su nombre";
+            cin>>nuevo.nombre;
+            cout<<"Ingrese su apellido";
+            cin>>nuevo.apellido;
+            cout<<"Ingrese su email";
+            cin>>nuevo.email;
+            cout<<"Ingrese su telefono";
+            cin>>nuevo.telefono;
+            cout<<"Ingrese su fecha de nacimiento";
+            cin>>nuevo.fechaNac;
+            nuevo.idCliente=Clientela.CantClientes+1;
+            nuevo.estado=0;
+            eResultados verificar;
+            verificar= AgregarCliente(Clientela, nuevo);
+            if(verificar==Exito){
+                cout<< "La operacion se realizó correctamente"<< endl;
+            }else {
+                    if(verificar==ErrExiste){
+                        cout<<"El cliente ya exite"<<endl;
+                    }else cout<< "Hubo un problema en el sistema. Disculpe las molestias. intente nuevamente"<<endl;
+                    }
+        }
+
+        if(variable==2) {//Modificar un cliente
+            int queQuiere;
+            sCliente Cliente;
+            cout<< "¿Cómo quiere buscar al cliente para modificarlo? (Ingrese un numero)"<<endl;
+            cout<< "1. Nombre y Apellido" <<endl << "2.El Id del cliente"<<endl <<"3.Email"<<endl;
+            cin>> queQuiere;
+            if(queQuiere==1){
+                cout<<"Ingrese el nombre"<<endl;
+                cin>>Cliente.nombre;
+                cout<<"Ingrese el apellido"<<endl;
+                cin>>Cliente.apellido;
+                BuscarCliente(Clientela,Cliente.nombre,Cliente.apellido,Cliente);
             }
+            if(queQuiere==2){
+                cout<<"Ingrese el id del cliente"<<endl;
+                cin>>Cliente.idCliente;
+                BuscarCliente(Clientela,Cliente.idCliente, Cliente);
+            }
+            if(queQuiere==3){
+                cout<<"Ingrese el email"<<endl;
+                cin>>Cliente.email;
+                BuscarCliente(Clientela,Cliente.email, Cliente);
+            }
+
         }
     }
 
